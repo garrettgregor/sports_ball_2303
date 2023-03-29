9b3a9aff51be92ee1eb66e12225d812be3fdfa91
Garrett Gregor <garrett.gregor@gmail.com>
2023-03-29 09:44:45 -0600

    Add total cost to player

--- lib/player.rb
+++ lib/player.rb
@@ -1,12 +1,14 @@
 class Player
   attr_reader :name,
               :monthly_cost,
-              :contract_length
+              :contract_length,
+              :total_cost,
 
   def initialize(name, monthly_cost, contract_length)
     @name = name
     @monthly_cost = monthly_cost
     @contract_length = contract_length
+    @total_cost = monthly_cost * contract_length
   end
 
   def first_name
