65c1df42580cee6fe31a6b79ce5cafc8c3cb495a
Garrett Gregor <garrett.gregor@gmail.com>
2023-03-29 10:00:00 -0600

    Add team spec and team class

---
+++ spec/team_spec.rb
@@ -0,0 +1,11 @@
+require "rspec"
+require "./lib/player"
+require "./lib/team"
+
+RSpec.describe Team do
+  it "exists" do
+    team = Team.new("Dodgers", "Los Angeles")
+
+    expect(team).to be_an_instance_of(Team)
+  end
+end
