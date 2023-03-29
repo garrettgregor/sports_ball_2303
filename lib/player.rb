5acce26c70e8ad936dad17d8bbf1e2c60bfccb37
Garrett Gregor <garrett.gregor@gmail.com>
2023-03-29 09:46:49 -0600

    Add nickname method to player

--- lib/player.rb
+++ lib/player.rb
@@ -3,12 +3,14 @@ class Player
               :monthly_cost,
               :contract_length,
               :total_cost,
+              :nickname
 
   def initialize(name, monthly_cost, contract_length)
     @name = name
     @monthly_cost = monthly_cost
     @contract_length = contract_length
     @total_cost = monthly_cost * contract_length
+    @nickname = nil
   end
 
   def first_name
